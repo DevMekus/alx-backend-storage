150feb318e723d878e790fa17514ea771431444c
Devmekus <nnaji.nnaemeka712@gmail.com>
2024-07-15 22:44:29 -0700

    NoSql tasks

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''Task 10
+'''
+
+
+def update_topics(mongo_collection, name, topics):
+    '''Function toChanges all topics of a 
+        collection's document based on the name.
+    '''
+    mongo_collection.update_many(
+        {'name': name},
+        {'$set': {'topics': topics}}
+    )
